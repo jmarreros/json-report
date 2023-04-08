8aa338450dcc3608e848902e794df6c869ee413d
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2023-04-08 16:31:16 -0500

    Agregado funcionalidad de javascript para iteraccion pantalla

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,1 +1,41 @@
-console.log('Hello World!');
+(function ($) {
+    'use strict';
+
+    $('.show-product').hide();
+
+    $('#add-product').click(function (e) {
+        e.preventDefault();
+        $('.product-container').last().clone().appendTo('.products-container');
+    });
+
+    $('#remove-product').click(function (e) {
+        e.preventDefault();
+        if ( $('.product-container').length > 1 ){
+            $('.product-container').last().remove();
+        }
+    });
+
+    $('.products-container').click(function (e) {
+        e.preventDefault();
+
+        if ( $(e.target).attr('class') === 'show-product' || $(e.target).attr('class')  === 'hide-product') {
+            $(e.target).hide();
+            if ($(e.target).attr('class') === 'show-product') {
+                $(e.target).parent().find('.hide-product').show();
+                $(e.target).closest('.product-container').find('.product-data').show();
+            } else {
+                $(e.target).parent().find('.show-product').show();
+                $(e.target).closest('.product-container').find('.product-data').hide();
+            }
+        }
+    });
+
+
+    $('.products-container').keyup(function (e) {
+        if ( $(e.target).attr('name') === 'ClaveProducto' ){
+            const text = $(e.target).val();
+            $(e.target).closest('.product-container').find('.product-code').text(text);
+        }
+    });
+
+})(jQuery);
--- src/views/front-end/client-form.php
+++ src/views/front-end/client-form.php
@@ -77,92 +77,56 @@
         </div>
     </div>
 
-    <div class="products-data" data-name="Producto">
 
-        <div class="product-toggle">
-            <a class="show-product" href="#">🔽</a>
-            <a class="hide-product" href="#">🔼</a>
-        </div>
+    <section class="products-container">
 
-        <div class="product-container">
+        <div class="product-container" data-name="Producto">
+            <div class="product-header">
+                <div><span class="product-code"></span></div>
+                <div class="product-toggle">
+                    <a class="show-product" href="#">🔽</a>
+                    <a class="hide-product" href="#">🔼</a>
+                </div>
+            </div>
 
-            <div class="product-general">
-                <div>
-                    <label>
-                        <span>Clave del Producto</span>
-                        <input name="ClaveProducto" type="text">
-                    </label>
+            <div class="product-data">
+
+                <div class="product-general">
+                    <div>
+                        <label>
+                            <span>Clave del Producto</span>
+                            <input name="ClaveProducto" type="text">
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            <span>Clave de Sub Producto</span>
+                            <input name="ClaveSubProducto" type="text">
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            <span>Densidad del Producto</span>
+                            <input name="DensidadDePetroleo" type="text">
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            <span>Composición de Azufre en el Petroleo (%)</span>
+                            <input name="ComposDeAzufreEnPetroleo" type="text">
+                        </label>
+                    </div>
                 </div>
 
-                <div>
-                    <label>
-                        <span>Clave de Sub Producto</span>
-                        <input name="ClaveSubProducto" type="text">
-                    </label>
-                </div>
-
-                <div>
-                    <label>
-                        <span>Densidad del Producto</span>
-                        <input name="DensidadDePetroleo" type="text">
-                    </label>
-                </div>
-
-                <div>
-                    <label>
-                        <span>Composición de Azufre en el Petroleo (%)</span>
-                        <input name="ComposDeAzufreEnPetroleo" type="text">
-                    </label>
-                </div>
             </div>
 
         </div>
 
-    </div>
+    </section>
 
-
-    <div class="products-data" data-name="Producto">
-
-        <div class="product-toggle">
-            <a class="show-product" href="#">🔽</a>
-            <a class="hide-product" href="#">🔼</a>
-        </div>
-
-        <div class="product-container">
-
-            <div class="product-general">
-                <div>
-                    <label>
-                        <span>Clave del Producto</span>
-                        <input name="ClaveProducto" type="text">
-                    </label>
-                </div>
-
-                <div>
-                    <label>
-                        <span>Clave de Sub Producto</span>
-                        <input name="ClaveSubProducto" type="text">
-                    </label>
-                </div>
-
-                <div>
-                    <label>
-                        <span>Densidad del Producto</span>
-                        <input name="DensidadDePetroleo" type="text">
-                    </label>
-                </div>
-
-                <div>
-                    <label>
-                        <span>Composición de Azufre en el Petroleo (%)</span>
-                        <input name="ComposDeAzufreEnPetroleo" type="text">
-                    </label>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
 
     <div>
         <input type="submit" value="Enviar">
